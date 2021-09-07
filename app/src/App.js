dbdfd30567b9f7818eb2cb51c6506d15dca43ef6
sandaru <sandarubro95@gmail.com>
2021-09-07 12:18:35 +0530

    Major Changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,8 @@
 import './App.css';
-import Todo from './components/Todo';
-import Modal from './components/modal';
-import Backdrop from './components/backdrop';
 
 function App() {
 	return (
-		<div>
-			<h1>My Todos</h1>
-			<Todo text="Learn React" />
-			<Todo text="Master React" />
-			<Todo text="Make Projects with React" />
-			<Todo text="Teach React" />
-		</div>
+		<div></div>
 	);
 }
 
